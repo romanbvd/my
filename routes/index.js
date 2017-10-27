1bfec234f49370907fadbff4bf9246d29816abee
roman <roman.bovdui@easternpeak.com>
2017-10-27 18:44:27 +0300

    Init commit

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: '/' });
+});
+
+router.get('/first_click', function(req, res, next) {
+    res.render('index', { title: 'first_click' });
+});
+
+router.get('/callback', function(req, res, next) {
+    res.render('index', { title: 'callback' });
+});
+
+router.get('/rejection', function(req, res, next) {
+    res.render('index', { title: 'rejection' });
+});
+
+router.get('/event', function(req, res, next) {
+    res.render('index', { title: 'event' });
+});
+
+router.get('/smartlink', function(req, res, next) {
+    res.render('index', { title: 'smartlink' });
+});
+
+router.get('/smartlink_redirect', function(req, res, next) {
+    res.render('index', { title: 'smartlink' });
+});
+
+module.exports = router;
